6aa4d7b4d232582bed92eee663b67f219314c3bd
Adi Tal <adital2464@gmail.com>
2023-08-16 15:00:49 +0300

    End of section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,22 +1,40 @@
+using System.Text;
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => 
-{
-   opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors();//cors
-
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+   .AddJwtBearer(options =>
+   {
+      options.TokenValidationParameters = new TokenValidationParameters
+      {
+         ValidateIssuerSigningKey = true,
+         IssuerSigningKey = new SymmetricSecurityKey(Encoding
+            .UTF8.GetBytes(builder.Configuration["TokenKey"])),
+         ValidateIssuer = false,
+         ValidateAudience = false
+      };
+   });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));//cors
+// Configure the HTTP request pipeline. - middlewares
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().
+   WithOrigins("http://localhost:4200"));//cors
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
